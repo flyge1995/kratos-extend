459ca1ab2b7d25c3b1bb227f21cd5f4ee8a96c32
随风 <76802345@qq.com>
2025-02-18 15:44:24 +0800

    init

---
+++ config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Option func(v *viper.Viper)
+
+func NewConfig(o ...Option) *Config {
+	v := viper.New()
+	v.AddConfigPath("configs")
+	v.SetConfigName("config")
+	v.SetConfigType("yaml")
+	for _, option := range o {
+		option(v)
+	}
+	return &Config{v: v}
+}
+
+type Config struct {
+	v *viper.Viper
+}
+
+func (c *Config) LoadEnvAndConfigFile(cfgFile string, cfgObj any) error {
+	if cfgFile != "" {
+		c.v.SetConfigFile(cfgFile)
+	}
+
+	c.v.AutomaticEnv()
+
+	err := c.v.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	err = c.v.Unmarshal(cfgObj)
+	return err
+}
