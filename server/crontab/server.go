f89b0a430037bcbf20c785a007757dfb2dcc4581
joy <76802345@qq.com>
2025-03-28 00:03:42 +0900

    fixbug

--- server/crontab/server.go
+++ server/crontab/server.go
@@ -2,6 +2,7 @@ package crontab
 
 import (
 	"context"
+	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/transport"
 	"github.com/robfig/cron/v3"
 )
@@ -10,18 +11,22 @@ var _ transport.Server = new(Server)
 
 type Server struct {
 	crontab *cron.Cron
+	logger  *log.Helper
 }
 
-func NewServer(crontab *cron.Cron) *Server {
-	return &Server{crontab: crontab}
+func NewServer(crontab *cron.Cron, logger log.Logger) *Server {
+	return &Server{crontab: crontab, logger: log.NewHelper(logger)}
 }
 
 func (s *Server) Start(ctx context.Context) error {
+	s.logger.Info("[crontab]server start")
 	s.crontab.Run()
+	s.logger.Info("[crontab]server stop")
 	return nil
 }
 
 func (s *Server) Stop(ctx context.Context) error {
+	s.logger.Info("[crontab]server stopping")
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
